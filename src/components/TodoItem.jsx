9a66986f416030d45e4b289542f205920f1731dd
Tim0124 <a0925172969@gmail.com>
2023-05-29 19:51:22 +0800

    Add save handle, add key down event when editing todo item

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,6 +5,7 @@ import {
   CheckHoverIcon,
 } from 'assets/images';
 import clsx from 'clsx'
+import { useRef } from 'react';
 
 const StyledTaskItem = styled.div`
   min-height: 52px;
@@ -102,14 +103,40 @@ const StyledTaskItem = styled.div`
 `;
 // todo.isEdit
 const TodoItem = ({todo, onToggleDone,onSave, onDelete, onChangeMode}) => {
+  const inputRef = useRef(null)
+  const handleKeyDown = (event) => {
+    if(inputRef.current.value.length > 0 && event.key === "Enter") {
+      onSave?.({id:todo.id, title:inputRef.current.value})
+    }
+    if(event.key === "Escape") {
+      onChangeMode?.({id:todo.id,isEdit:false})
+    }
+  }
   return (
-    <StyledTaskItem className={clsx('',{done: todo.isDone, edit: todo.isEdit })}>
+    <StyledTaskItem
+      className={clsx('', { done: todo.isDone, edit: todo.isEdit })}
+    >
       <div className="task-item-checked">
-        <span className="icon icon-checked" onClick={() =>{onToggleDone?.(todo.id)}}/>
+        <span
+          className="icon icon-checked"
+          onClick={() => {
+            onToggleDone?.(todo.id);
+          }}
+        />
       </div>
-      <div className="task-item-body" onDoubleClick={() => {onChangeMode?.({id: todo.id, isEdit:true})}}>
+      <div
+        className="task-item-body"
+        onDoubleClick={() => {
+          onChangeMode?.({ id: todo.id, isEdit: true });
+        }}
+      >
         <span className="task-item-body-text">{todo.title}</span>
-        <input className="task-item-body-input" value={todo.title}/>
+        <input
+          ref={inputRef}
+          className="task-item-body-input"
+          onKeyDown={handleKeyDown}
+          defaultValue={todo.title}
+        />
       </div>
       <div className="task-item-action ">
         <button className="btn-reset btn-destroy icon"></button>
