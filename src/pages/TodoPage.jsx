4644c964757686566612a731eca7e32969649383
Tim0124 <a0925172969@gmail.com>
2023-05-29 18:48:38 +0800

    Add state to store todos, save button event handler

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -27,17 +27,40 @@ const dummyTodos = [
 
 const TodoPage = () => {
   const [inputValue, setInputValue] = useState('')
+  const [todos, setTodos] = useState(dummyTodos)
 
   const handleInput = (Value) => {
     setInputValue(Value);
   };
 
+
+  const handleTodo = () => {
+    if(inputValue.length === 0) {
+      return
+    }
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        {
+          id:Math.random() * 100,
+          title: inputValue,
+          isDone: false,
+        }
+      ]
+    })
+    setInputValue('')
+  }
+
   return (
     <div>
       TodoPage
       <Header />
-      <TodoInput inputValue={inputValue} onChange={handleInput}/>
-      <TodoCollection todos={dummyTodos} />
+      <TodoInput
+        inputValue={inputValue}
+        onChange={handleInput}
+        onAddTodo={handleTodo}
+      />
+      <TodoCollection todos={todos} />
       <Footer />
     </div>
   );
